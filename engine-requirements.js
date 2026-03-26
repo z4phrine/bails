1ccf85e7b2d1226ce0819fa2818d025ab291ae40
z4phrinee <kenzoepep681@gmail.com>
2026-03-26 18:48:16 +0700

    Add files via upload

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,10 +1,10 @@
 const major = parseInt(process.versions.node.split('.')[0], 10);
 
-if (major < 20) {
+if (major < 24) {
   console.error(
-    '[Error] Node.js 20+ is required to run baileys\n' +
+    '\n[Error] Node.js 24+ is required to run bianoffc.\n' +
     'Current version is Node.js ' + process.versions.node + '.\n' +
-    'Please upgrade to Node.js 20+ to proceed.\n'
+    'Please upgrade to Node.js 24+ to proceed.\n'
   );
   process.exit(1);
 }
