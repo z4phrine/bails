cd9913b74ccd6521d2a342660a9f18e343733e80
z4phrinee <kenzoepep681@gmail.com>
2026-03-26 18:48:42 +0700

    Update engine-requirements.js

--- engine-requirements.js
+++ engine-requirements.js
@@ -2,7 +2,7 @@ const major = parseInt(process.versions.node.split('.')[0], 10);
 
 if (major < 24) {
   console.error(
-    '\n[Error] Node.js 24+ is required to run bianoffc.\n' +
+    '[Error] Node.js 24 is required to run the baileys\n' +
     'Current version is Node.js ' + process.versions.node + '.\n' +
     'Please upgrade to Node.js 24+ to proceed.\n'
   );
