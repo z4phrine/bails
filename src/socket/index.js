1547a2cb9d34384a23e2a9939b69c0e5b3331314
z4phrinee <kenzoepep681@gmail.com>
2026-03-26 10:34:36 +0700

    Create index.js

---
+++ src/socket/index.js
@@ -0,0 +1,13 @@
+import { DEFAULT_CONNECTION_CONFIG } from "../defaults/index.js";
+import { makeCommunitiesSocket } from "./communities.js";
+const makeWASocket = (config) => {
+  const newConfig = {
+    ...DEFAULT_CONNECTION_CONFIG,
+    ...config
+  };
+  if (config.shouldSyncHistoryMessage === undefined) {
+    newConfig.shouldSyncHistoryMessage = () => !!newConfig.syncFullHistory;
+  }
+  return makeCommunitiesSocket(newConfig);
+};
+export default makeWASocket;
